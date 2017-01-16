fba39be7e20e1a6e8932a34c9082be7db09e9567
github username <github email address>
2017-01-16 12:49:58 -0500

    initial pass

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,10 @@
+require 'byebug'
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  charArray = characters_to_sort.split('')
+  finalArr = (long_text.split(''))
+  finalArr.delete_if { |char| !charArray.include?(char) }
+  finalArr = finalArr.group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
+  charArray.each { |char| finalArr.push(char) if finalArr.include?(char) == false}
+  byebug
+  finalArr.join('')
 end
